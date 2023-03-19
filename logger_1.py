e2859a915beb57ee611d979197ffe474bc2af649
Anastasia Bocharova <poisonsweets@mail.ru>
2023-03-19 20:42:37 +0300

    Initial commit

---
+++ logger_1.py
@@ -0,0 +1,64 @@
+import os
+from datetime import datetime
+import json
+
+
+def logger(old_function):
+
+    def new_function(*args, **kwargs):
+        result = old_function(*args, **kwargs)
+        log = {
+               'name': old_function.__name__,
+               'datetime': datetime.now(),
+               'args': args,
+               'kwargs': kwargs,
+               'return': result
+               }
+
+        with open('main.log', 'a', encoding='utf-8') as file:
+            json.dump(log, file, ensure_ascii=False, indent='\t', default=str)
+
+        return result
+
+    return new_function
+
+
+def test_1():
+    path = 'main.log'
+    if os.path.exists(path):
+        os.remove(path)
+
+    @logger
+    def hello_world():
+        return 'Hello World'
+
+    @logger
+    def summator(a, b=0):
+        return a + b
+
+    @logger
+    def div(a, b):
+        return a / b
+
+    assert 'Hello World' == hello_world(), "Функция возвращает 'Hello World'"
+    result = summator(2, 2)
+    assert isinstance(result, int), 'Должно вернуться целое число'
+    assert result == 4, '2 + 2 = 4'
+    result = div(6, 2)
+    assert result == 3, '6 / 2 = 3'
+
+    assert os.path.exists(path), 'файл main.log должен существовать'
+
+    summator(4.3, b=2.2)
+    summator(a=0, b=0)
+
+    with open(path) as log_file:
+        log_file_content = log_file.read()
+
+    assert 'summator' in log_file_content, 'должно записаться имя функции'
+    for item in (4.3, 2.2, 6.5):
+        assert str(item) in log_file_content, f'{item} должен быть записан в файл'
+
+
+if __name__ == '__main__':
+    test_1()
---
+++ logger_2.py
@@ -0,0 +1,71 @@
+from time import asctime
+import os
+
+
+def logger(path):
+    path = os.path.basename(path)
+
+    def __logger(old_function):
+
+        def new_function(*args, **kwargs):
+            result = old_function(*args, **kwargs)
+            log = {
+                'name': old_function.__name__,
+                'datetime': asctime(),
+                'args': args,
+                'kwargs': kwargs,
+                'return': result}
+
+            with open(path, 'a', encoding='utf-8') as file:
+                file.write(str(log))
+                file.write("\n")
+
+            return result
+
+        return new_function
+
+    return __logger
+
+
+def test_2():
+    paths = ('log_1.log', 'log_2.log', 'log_3.log')
+
+    for path in paths:
+        if os.path.exists(path):
+            os.remove(path)
+
+        @logger(path)
+        def hello_world():
+            return 'Hello World'
+
+        @logger(path)
+        def summator(a, b=0):
+            return a + b
+
+        @logger(path)
+        def div(a, b):
+            return a / b
+
+        assert 'Hello World' == hello_world(), "Функция возвращает 'Hello World'"
+        result = summator(2, 2)
+        assert isinstance(result, int), 'Должно вернуться целое число'
+        assert result == 4, '2 + 2 = 4'
+        result = div(6, 2)
+        assert result == 3, '6 / 2 = 3'
+        summator(4.3, b=2.2)
+
+    for path in paths:
+
+        assert os.path.exists(path), f'файл {path} должен существовать'
+
+        with open(path) as log_file:
+            log_file_content = log_file.read()
+
+        assert 'summator' in log_file_content, 'должно записаться имя функции'
+
+        for item in (4.3, 2.2, 6.5):
+            assert str(item) in log_file_content, f'{item} должен быть записан в файл'
+
+
+if __name__ == '__main__':
+    test_2()
